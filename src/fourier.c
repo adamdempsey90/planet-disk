92f4b44b57959b4f1b9328a8be859010329ce200
Adam Dempsey <adamdemps@gmail.com>
2014-07-30 14:43:36 -0500

    starting restructuring

--- src/fourier.c
+++ src/fourier.c
@@ -47,7 +47,8 @@ int main(void) {
 	Nx = fld->Nx;
 	Ny = fld->Ny;
 	Nk = fld->Nk;
-	double complex *uv = (double complex *)malloc(sizeof(double complex)*Nx*NC);
+
+	double complex *uv = (double complex *)malloc(sizeof(double complex)*NTOTC);
 
 	
 	
@@ -64,8 +65,8 @@ int main(void) {
 		for(j=0;j<NC;j++) {
 			fprintf(rfp1,"%lg ",creal(fld->u[CINDX]));
 			fprintf(cfp1,"%lg ",cimag(fld->u[CINDX]));
-			fprintf(rfp1,"%lg ",creal(fld->v[CINDX]));
-			fprintf(cfp1,"%lg ",cimag(fld->v[CINDX]));
+			fprintf(rfp2,"%lg ",creal(fld->v[CINDX]));
+			fprintf(cfp2,"%lg ",cimag(fld->v[CINDX]));
 		}
 		fprintf(rfp1,"\n"); fprintf(cfp1,"\n");
 		fprintf(rfp2,"\n"); fprintf(cfp2,"\n");
@@ -107,7 +108,7 @@ void convolve(double complex *q1, double complex *q2, double complex *res, int N
 	fftw_execute(c2r2);
 
 /* Form product in real space */
-	for(i=0;i<NTOTR;i++) wr3[i] = wr1[i]*wr2[i];
+	for(i=0;i<NTOTR;i++) wr3[i] = wr1[i]*wr2[i]/(Ny*Ny);
 
 /* FFT back to complex space */
 	fftw_execute(r2c3);
@@ -128,8 +129,8 @@ void init_Field(Field *fld) {
 	fld->x = (double *)malloc(sizeof(double)*fld->Nx);
 	fld->y = (double *)malloc(sizeof(double)*fld->Ny);
 	fld->k = (double *)malloc(sizeof(double)*NC);
-	fld->u = (double complex *)malloc(sizeof(double complex)*NC);
-	fld->v = (double complex *)malloc(sizeof(double complex)*NC);
+	fld->u = (double complex *)malloc(sizeof(double complex)*NTOTC);
+	fld->v = (double complex *)malloc(sizeof(double complex)*NTOTC);
 	fld->vx = (double *)fld->u;
 	fld->vy = (double *)fld->v;
 	
@@ -137,13 +138,12 @@ void init_Field(Field *fld) {
 	for(i=0;i<fld->Ny;i++) fld->y[i] = -15 + i*30.0/fld->Ny;
 	for(i=0;i<NC;i++) fld->k[i] = i*2*M_PI/30.0;
 
-	for(i=0;i<fld->Nx;i++) {
+	for(i=0;i<Nx;i++) {
 		x = fld->x[i];
 		for(j=0;j<NC;j++) {
 			k = fld->k[j];
-			ld = M_PI/pow(k*k*.0024,1./3);
-			fld->u[CINDX] = csqrt(x)*cexp((2.0/(3.0*k))*I*x*x)*exp(-pow(x/ld,3.));
-			fld->v[CINDX] = cexp((2.0/(3.0*k))*I*x*x)*exp(-pow(x/ld,3.))/csqrt(x);	
+			fld->u[CINDX] = cexp(I*k*k*x*x);
+			fld->v[CINDX] = cexp(-I*k*k*x*x);
 		}
 	}
 
@@ -165,37 +165,37 @@ void init_Field(Field *fld) {
 }
 void init_fft(int Nx, int Ny) {
 	int i,j,Nmax;
-	wc1 = (double complex *)malloc(sizeof(double complex)*Nx*NC);
+	wc1 = (double complex *)malloc(sizeof(double complex)*NTOTC);
 	wr1 = (double *)wc1;
-	wc2 = (double complex *)malloc(sizeof(double complex)*Nx*NC);
+	wc2 = (double complex *)malloc(sizeof(double complex)*NTOTC);
 	wr2 = (double *)wc2;
-	wc3 = (double complex *)malloc(sizeof(double complex)*Nx*NC);
+	wc3 = (double complex *)malloc(sizeof(double complex)*NTOTC);
 	wr3 = (double *)wc3;
-	mask = (double *)malloc(sizeof(double)*Nx*NC);
+	mask = (double *)malloc(sizeof(double)*NTOTC);
 	
-	Nmax = (2./3)*NC;
+	Nmax = (2./3)*(NC-1);
 	
 	for(i=0;i<Nx;i++) {
 		for(j=0;j<NC;j++) {
-			if (j <= Nmax) mask[CINDX] = 1;
+			if (j < Nmax) mask[CINDX] = 1;
 			else mask[CINDX] = 0;
 		}
 	}
 	
 	c2r1=fftw_plan_many_dft_c2r(1,&Nx,Ny,wc1,&Nx,1,NC,wr1,&Nx,1,NR,FFTW_ESTIMATE);
-	if (c2r == NULL) printf("Problem with c2r\n");
+	if (c2r1 == NULL) printf("Problem with c2r1\n");
 	r2c1=fftw_plan_many_dft_r2c(1,&Nx,Ny,wr1,&Nx,1,NR,wc1,&Nx,1,NC,FFTW_ESTIMATE);
-	if (r2c == NULL) printf("Problem with r2c\n");
+	if (r2c1 == NULL) printf("Problem with r2c1\n");
 	
 	c2r2=fftw_plan_many_dft_c2r(1,&Nx,Ny,wc2,&Nx,1,NC,wr2,&Nx,1,NR,FFTW_ESTIMATE);
-	if (c2r == NULL) printf("Problem with c2r\n");
+	if (c2r2 == NULL) printf("Problem with c2r2\n");
 	r2c2=fftw_plan_many_dft_r2c(1,&Nx,Ny,wr2,&Nx,1,NR,wc2,&Nx,1,NC,FFTW_ESTIMATE);
-	if (r2c == NULL) printf("Problem with r2c\n");
+	if (r2c2 == NULL) printf("Problem with r2c2\n");
 	
 	c2r3=fftw_plan_many_dft_c2r(1,&Nx,Ny,wc3,&Nx,1,NC,wr3,&Nx,1,NR,FFTW_ESTIMATE);
-	if (c2r == NULL) printf("Problem with c2r\n");
+	if (c2r3 == NULL) printf("Problem with c2r3\n");
 	r2c3=fftw_plan_many_dft_r2c(1,&Nx,Ny,wr3,&Nx,1,NR,wc3,&Nx,1,NC,FFTW_ESTIMATE);
-	if (r2c == NULL) printf("Problem with r2c\n");
+	if (r2c3 == NULL) printf("Problem with r2c3\n");
 	
 	
 	return;
